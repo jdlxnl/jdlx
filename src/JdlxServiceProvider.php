f6427bbec04c350d092c4f4a7311391e5aaee891
Petar Zelyamov <elessar4531@me.com>
2022-01-27 23:58:15 +0200

    Assign role to a user command.

--- src/JdlxServiceProvider.php
+++ src/JdlxServiceProvider.php
@@ -7,6 +7,7 @@ use Jdlx\Commands\Account\AccountChangePasswordCommand;
 use Jdlx\Commands\Account\AccountCreateCommand;
 use Jdlx\Commands\Auth\LoginSanctumCommand;
 use Jdlx\Commands\Docs\GenerateDocsCommand;
+use Jdlx\Commands\Account\AccountAssignRoleCommand;
 
 
 class JdlxServiceProvider extends ServiceProvider
@@ -25,6 +26,7 @@ class JdlxServiceProvider extends ServiceProvider
                 AccountCreateCommand::class,
                 LoginSanctumCommand::class,
                 AccountCreateCommand::class,
+                AccountAssignRoleCommand::class,
             ]);
         }
 
